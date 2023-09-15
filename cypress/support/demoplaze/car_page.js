48215d0353733ebfc3437f00347c3c910139033e
Gustavo Huanca <gustavozha@gmail.com>
2023-09-15 06:17:44 -0400

    Add test: 'Add and delete a product' with the verification in cart

--- cypress/support/demoplaze/car_page.js
+++ cypress/support/demoplaze/car_page.js
@@ -1,6 +1,5 @@
 /// <reference types="cypress" />
 import { removeItem } from "../utils/utils";
-import { getTitleProduct } from "../utils/utils";
 
 export class CarPage {
     x_icon = '.table-responsive a[onclick]'
@@ -18,19 +17,49 @@ export class CarPage {
 
     verifyProduct(productTitle) {
         var recordDetails = [];
-        cy.log(productTitle.length)
         cy.get(this.record_table + ' td:nth-child(2)').each(($el, index, $list) => {
             recordDetails[index] = ($el.text());
-            cy.log(recordDetails[index])
         })
-        .then(() => {  
-            var i;
-            for (i = 0; i < productTitle.length; i++) {
-           
-            recordDetails = removeItem(recordDetails, productTitle[i])
-                
+            .then(() => {
+                var i;
+                for (i = 0; i < productTitle.length; i++) {
+                    recordDetails = removeItem(recordDetails, productTitle[i])
+                }
+                expect(recordDetails.length == 0).to.be.true
+            })
+    }
+
+    deleteProduct(productName) {
+        var indexToClick;
+        cy.get(this.record_table + ' td:nth-child(2)').each(($el, index, $list) => {
+            if ($el.text().includes(productName)) {
+                cy.get(this.record_table + ' td a').eq(index).click()
             }
-            expect(recordDetails.length== 0).to.be.true
         })
+    }
+
+    verifyProductIsDeleted(productTitle) {
+        var recordDetails = [];
+        var productExits = false;
+
+        cy.get('#tbodyid tr')
+            .should(Cypress._.noop)
+            .its('length')
+            .then((n) => {
+                if (n == 0) {
+                    expect(productExits).to.be.false
+                } else {
+                    cy.get(this.record_table + ' td:nth-child(2)').each(($el, index, $list) => {
+                        recordDetails[index] = ($el.text());
+                    })
+                        .then(() => {
+                            var i;
+                            for (i = 0; i < recordDetails.length; i++) {
+                                if (recordDetails[i] == productTitle) productExits = true;
+                            }
+                            expect(productExits).to.be.false
+                        })
+                }
+            })
     }
 }
